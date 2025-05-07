54a80f6d9dd10c9459bb7eebac2eed1c6de8af47
Shishank Sharma <shishanksharma@Shishanks-MacBook-Air.local>
2025-05-07 18:26:45 +0530

    First Commit

---
+++ src/main/java/com/crozhere/service/club/CrozhereClubApplication.java
@@ -0,0 +1,13 @@
+package com.crozhere.service.club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrozhereClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrozhereClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crozhere/service/club/CrozhereClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crozhere.service.club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrozhereClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
